729c6893a092b85acb2dc431ddb47b79a0c48aa7
Calvin2412 <2119777@talnet.nl>
2021-01-27 23:44:39 +0100

    index.php

---
+++ FlowerPower/index.php
@@ -0,0 +1,18 @@
+<?php
+
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">FlowerPower</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Inloggen medewerkers</a></li>
+      <li><a href="#">Contactpagina</a></li>
+      <li><a href="#">Inloggen klanten</a></li>
+      <li><a href="#">Registreren</a></li>
+    </ul>
+  </div>
+</nav>
+
+ ?>
